ff08ca7f291fa572426d195bae3563217e9235bb
yusufcakal <info@yusufcakal.com>
2018-01-25 17:10:33 +0300

    remove name

--- src/main/java/com/yusufcakal/userapp/repository/UserRepository.java
+++ src/main/java/com/yusufcakal/userapp/repository/UserRepository.java
@@ -3,9 +3,6 @@ package com.yusufcakal.userapp.repository;
 import com.yusufcakal.userapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by rajeevkumarsingh on 27/06/17.
- */
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
