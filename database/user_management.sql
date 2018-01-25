0d062c8f975a0d3b0d1d98e601f8b8598f34c94f
yusufcakal <info@yusufcakal.com>
2018-01-25 17:24:23 +0300

    front-end and db added

---
+++ database/user_management.sql
@@ -0,0 +1,23 @@
+﻿# Host: 127.0.0.1  (Version 5.7.15-log)
+# Date: 2018-01-25 15:17:07
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `book` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (43,'ba12','b',2),(44,'barasdasdasdas','barasdasd',2112);
---
+++ front-end/userapp/userapp/js/script.js
@@ -0,0 +1,160 @@
+$(document).ready(function(){
+
+    $("#update").hide();
+
+    assignDataToTable();
+
+    $('table').on('click', 'button[id="delete"]', function(e){
+       var id = $(this).closest('tr').children('td:first').text();
+       
+       $.ajax({
+            type:"DELETE",
+            url:"http://localhost:8080/api/users/" + id,
+            success: function(data){
+                alertUsing("Silindi.", true);
+                assignDataToTable();
+            },
+            error: function(err) {  
+                console.log(err);
+                alert(err);
+            }
+        });
+
+    })
+
+    $('table').on('click', 'button[id="edit"]', function(e){
+       var id = $(this).closest('tr').children('td:first').text();
+       var name = $(this).closest('tr').children('td:nth-child(2)').text(); 
+       var age = $(this).closest('tr').children('td:nth-child(3)').text(); 
+       var book = $(this).closest('tr').children('td:nth-child(4)').text(); 
+
+        $("#name").val(name);
+        $("#age").val(age);
+        $("#book").val(book);
+
+        $("#update").show();
+        $("#save").hide();
+
+        $("#update").click(function() {
+
+            var ageNum = parseInt($("#age").val());
+
+            var jsonVar = {
+                name: $("#name").val(),
+                age: ageNum,
+                book: $("#book").val()
+            };
+
+            $.ajax({
+                type:"PUT",
+                data: JSON.stringify(jsonVar),
+                contentType: "application/json",
+                url:"http://localhost:8080/api/users/" + id,
+                success: function(data){
+                    alertUsing("Düzenlendi.", true);
+                    $("#update").hide();
+                    $("#save").show();
+                    $("#name").val("");
+                    $("#age").val("");
+                    $("#book").val("");
+                    assignDataToTable();
+                },
+                error: function(err) {  
+                    console.log(err);
+                    alert(err);
+                }
+
+        });
+
+    });
+
+    })
+
+    var age = $("#age");
+
+    age.keypress(function(key){
+        if(key.charCode > 48 && key.charCode < 57){ // Girilen değer sayı ise
+            if(age.val().length < 3){
+                return true;
+            }else{
+                alertUsing("3 Haneyi Aştınız.", false);
+                return false;
+            }
+        }else{
+            alertUsing("Sayı Giriniz.", false);
+            return false; // input'a girilmesine izin verme.
+        }
+    });
+
+    $("#save").click(function() {
+
+        var jsonVar = {
+            name: $("#name").val(),
+            age: $("#age").val(),
+            book: $("#book").val()
+        };
+
+        $.ajax({
+            type:"POST",
+            url:"http://localhost:8080/api/users",
+            data: JSON.stringify(jsonVar),
+            contentType: "application/json",
+            success: function(data){
+                assignDataToTable();
+            },
+            error: function(err) {
+                console.log(err);
+                alert(err);
+            }
+        });
+
+    });
+
+    function assignDataToTable() {
+        $("tbody").empty();
+        $.ajax({    
+          type:"GET",
+          contentType: "application/json",
+          url:"http://localhost:8080/api/users",
+          success: function(data) {
+            var users = JSON.parse(JSON.stringify(data));
+            for (var i in users) {
+                $("tbody").
+                append("<tr> \
+                            <td>" +  users[i].id + "</td> \
+                            <td>" +  users[i].name + "</td> \
+                            <td>" +  users[i].age + "</td> \
+                            <td>" +  users[i].book + "</td> \
+                            <td> \ <button id='delete' class='btn btn-danger'>Sil</button> \
+                           <button id='edit' class='btn btn-warning'>Düzenle</button> \ </td> \
+                        </tr>");
+            }
+          },
+          error: function(data) { 
+            console.log(data);
+            }
+        });
+       
+    }
+
+function alertUsing(text, flag) {
+
+    var alert = $(".alert");
+
+    if(flag){
+        alert.removeClass("alert-danger").addClass("alert-success");
+    }else{
+        alert.removeClass("alert-success").addClass("alert-danger");
+        
+    }
+    
+    alert.fadeIn(400);
+    alert.css("display", "block");
+    alert.text(text);
+    setTimeout(function() {
+        alert.fadeOut();
+    }, 2000);
+
+  }
+
+});
